eddfd477a32646ddb3b11f5a75c5663d6c82f081
TylerTaylor <vikingtyty@gmail.com>
2023-03-23 08:40:40 -0500

    Initial commit

---
+++ server/app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from flask import Flask, make_response, jsonify, request
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+
+from models import db, Restaurant, RestaurantPizza, Pizza
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+api = Api(app)
+
+@app.route('/')
+def index():
+    return '<h1>Code challenge</h1>'
+
+@app.route('/restaurants')
+def restaurants():
+
+    pass
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
