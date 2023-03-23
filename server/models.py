87317cf120831039655dd8eff482266f1df6d86a
Tim <weberk76@gmail.com>
2023-03-23 11:56:50 -0400

    need more study time

--- server/models.py
+++ server/models.py
@@ -11,3 +11,34 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+
+class RestaurantPizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'))
+    price = db.Column(db.Float, nullable=False)
+    @validates('price')
+    def validate_price(self, key, value):
+        if not (1 <= value <= 30):
+            raise ValueError('Price must be between 1 and 30.')
+        return value
+    restaurant = db.relationship('Restaurant', backref='restaurant_pizzas')
+    pizza = db.relationship('Pizza', backref='restaurant_pizzas')
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+# create Restaurant table
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+
+# create Pizza table
+class Pizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    
